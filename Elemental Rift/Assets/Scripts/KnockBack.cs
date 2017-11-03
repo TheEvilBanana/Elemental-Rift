d6b7f9173f4c55406ba9533c77b6eaa99d56cffd
Ruchita Kondvilkar <ruchi3030@outlook.com>
2017-11-02 21:38:24 -0400

    Ground Attack in Test1 scene

---
+++ Elemental Rift/Assets/Scripts/KnockBack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour {
+
+    // Use this for initialization
+    public ParticleSystem fissureAttack;
+    public Transform playerTransform;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            fissureAttack.transform.position = playerTransform.position + playerTransform.forward * 5;
+            fissureAttack.Play();
+        }
+		
+	}
+}
