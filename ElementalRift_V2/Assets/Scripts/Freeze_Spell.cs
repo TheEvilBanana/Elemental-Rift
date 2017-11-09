8d0d44d047669905e8a5661b7ef7cb8ca9c610d2
Ruchita Kondvilkar <ruchi3030@outlook.com>
2017-11-09 16:39:33 -0500

    Imported Assets

---
+++ ElementalRift_V2/Assets/Scripts/Freeze_Spell.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Freeze_Spell : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField]
+
+    private ParticleSystem[] freezePartcileSystem;
+
+    private Vector3 mousePos;
+ 
+
+	void Start ()
+    {
+
+        freezePartcileSystem = gameObject.GetComponentsInChildren<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(0))
+        { 
+            foreach (ParticleSystem childParticleSystem in freezePartcileSystem)
+            {
+                childParticleSystem.Play();
+                mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Debug.Log(mousePos);
+                childParticleSystem.transform.position = mousePos;    
+                //Instantiate(childParticleSystem, new Vector3(0, 1, 0), Quaternion.identity);    
+            }
+        }
+	}
+
+    
+}
---
+++ ElementalRift_V2/Assets/Scripts/KnockBack_Spell.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack_Spell : MonoBehaviour {
+    [SerializeField]
+    private ParticleSystem[] knockBackParticleSystem;
+
+    private GameObject Player;
+    
+	void Start ()
+    {
+        knockBackParticleSystem = gameObject.GetComponentsInChildren<ParticleSystem>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.E))
+        {
+            foreach(ParticleSystem childParticleSystem in knockBackParticleSystem)
+            {
+                childParticleSystem.Play();                
+            }
+
+           
+        }
+	}
+}
