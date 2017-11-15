fadaf30180bf47e85101a7eb029e797f2ef3cb5f
Ruchita Kondvilkar <ruchi3030@outlook.com>
2017-11-14 19:51:20 -0500

    Player and camera movement

---
+++ ElementalRift_V2/Assets/Scripts/Gun.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera camera;
+    public GameObject freezeEffect;
+    public GameObject knockBackEffect;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        //For the left mouse click play - the freeze attack
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+        //For Alpha1 playe the ground - knockback attack
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            KnockBack();
+        }
+    }
+
+    //For the freeze attack
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform);
+            Enemy target = hit.transform.GetComponent<Enemy>();
+
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            GameObject impactGO = Instantiate(freezeEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+
+    //For the knockback attack
+    void KnockBack()
+    {
+        GameObject knock = Instantiate(knockBackEffect, camera.transform.position, camera.transform.rotation);
+        Destroy(knock, 2f);
+    }
+}
--- ElementalRift_V2/Assets/Scripts/Player_Movement.cs
+++ ElementalRift_V2/Assets/Scripts/Player_Movement.cs
@@ -2,70 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Movement : MonoBehaviour {
+public class Player_Movement : MonoBehaviour
+{
 
-    // Use this for initialization
-    public static CharacterController PlayerCharacterController;
-    private float moveSpeed = 10f;
-
-    public Vector3 MoveVector { get; set; }
-
-    private void Awake()
+    private CrossHair _Crosshair;
+    private CrossHair Crosshair
     {
-        PlayerCharacterController = GetComponent<CharacterController>() as CharacterController;
-    }
-
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        GetInput();
-
-        SnapAlignCharacterWithCamera();
-
-        ProcessMotion();
-	}
-
-    void GetInput()
-    {
-        var deadZone = 0.1f;
-
-        MoveVector = Vector3.zero;
-
-        if (Input.GetAxis("Vertical") > deadZone || Input.GetAxis("Vertical") < deadZone)
-            MoveVector += new Vector3(0, 0, Input.GetAxis("Vertical"));
-
-        if (Input.GetAxis("Horizontal") > deadZone || Input.GetAxis("Horizontal") < deadZone)
-            MoveVector += new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-    }
-
-    void SnapAlignCharacterWithCamera()
-    {
-        if(MoveVector.x!=0 || MoveVector.z!=0)
+        get
         {
-            transform.rotation = Quaternion.Euler(transform.eulerAngles.x, Camera.main.transform.eulerAngles.y, transform.eulerAngles.z);
+            if (_Crosshair == null)
+                _Crosshair = GetComponentInChildren<CrossHair>();
+            return _Crosshair;
         }
     }
 
-    void ProcessMotion()
+
+
+    public float speed;
+    public Vector2 Damping;
+    public Vector2 Sensitivity;
+
+    private Player_Movement localPlayer;
+    Vector2 mouseInput;
+
+    private void Awake()
     {
-        //Transform MoveVector to WorldSpace
-        MoveVector = transform.TransformDirection(MoveVector);
-
-        //Normalze MoveVector if Magnitude >1
-        if (MoveVector.magnitude > 1)
-            MoveVector = Vector3.Normalize(MoveVector);
-
-        //Multiply MoveVector by moveSpeed
-        MoveVector *= moveSpeed;
-
-        //Multiply MoveVector by deltaTime
-        MoveVector *= Time.deltaTime;
-
-        //Move the character in world space
-        PlayerCharacterController.Move(MoveVector);
+        localPlayer = this;
     }
+
+    private void Update()
+    {
+        Vector2 direction = new Vector2(Input.GetAxis("Vertical") * speed, Input.GetAxis("Horizontal") * speed);
+        transform.position += transform.forward * direction.x * Time.deltaTime + transform.right * direction.y * Time.deltaTime;
+
+        var x = Input.GetAxisRaw("Mouse X");
+        var y = Input.GetAxisRaw("Mouse Y");
+
+        mouseInput.x = Mathf.Lerp(mouseInput.x, x, 1f / Damping.x);
+        mouseInput.y = Mathf.Lerp(mouseInput.y, y, 1f /Damping.y);
+
+        transform.Rotate(Vector3.up * mouseInput.x * Sensitivity.x);
+
+        Crosshair.LookHeight(mouseInput.y * Sensitivity.y);
+
+    }
+
 }
